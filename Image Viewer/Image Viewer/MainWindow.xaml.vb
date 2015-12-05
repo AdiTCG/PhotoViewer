bc01431044cf8a5f848844a72bc6fc82564f3fc1
AdiTCG <adithecomputerguy@gmail.com>
2015-12-06 02:32:56 +0530

    Created a ignore file to ignore c# project in visual studio

--- Image Viewer/Image Viewer/MainWindow.xaml.vb
+++ Image Viewer/Image Viewer/MainWindow.xaml.vb
@@ -10,50 +10,50 @@ Class MainWindow
 		ImgContentCtrl.RenderTransform = myScale
 	End Sub
 
-	Private Sub ImgThumb_DragDelta(ByVal sender as Object, ByVal e as System.Windows.Controls.Primitives.DragDeltaEventArgs)
-		'TODO: Add event handler implementation here.
-		
-		Dim left as Double = Canvas.GetLeft(ImgContentCtrl)
-		Dim top as Double = Canvas.GetTop(ImgContentCtrl)
-		
-		Canvas.SetLeft(ImgContentCtrl, (left + e.HorizontalChange))
-		Canvas.SetTop(ImgContentCtrl, (top + e.VerticalChange))
-	End Sub
+    Private Sub Img_DragDelta(ByVal sender As Object, ByVal e As System.Windows.Controls.Primitives.DragDeltaEventArgs)
+        'TODO: Add event handler implementation here.
 
-	Private Sub ImgThumb_MouseWheel(ByVal sender as Object, ByVal e as System.Windows.Input.MouseWheelEventArgs)
-		'TODO: Add event handler implementation here.
-		
-		' Variable for holding the mouse's delta value.
-		Dim deltaValue as Integer
-		
-		deltaValue = e.Delta
-		
-		' Set the center point of the ScaleTransform object
-		' to the cursor location.
-		myScale.CenterX = e.GetPosition(ImgContentCtrl).X
-		myScale.CenterY = e.GetPosition(ImgContentCtrl).Y
-		
-		' Zoom in when the user scrolls the mouse wheel up
-		' and vice versa.
-		If deltaValue > 0 Then
-			' Limit zoom-in to 500%
-			If myScale.ScaleX < 5 Then
-				' Zoom-in in 10% increments
-				myScale.ScaleX += 0.1
-				myScale.ScaleY += 0.1			
-			End If
-		' When mouse wheel is scrolled down...
-		Else
-			' Limit zoom-out to 80%
-			If myScale.ScaleX > 0.8 Then
-				' Zoom-out by 10%
-				myScale.ScaleX -= 0.1
-				myScale.ScaleY -= 0.1
-			End If
-		End If
-	End Sub
+        Dim left As Double = Canvas.GetLeft(ImgContentCtrl)
+        Dim top As Double = Canvas.GetTop(ImgContentCtrl)
 
-	Private Sub OpenButton_Click(ByVal sender as Object, ByVal e as System.Windows.RoutedEventArgs)
+        Canvas.SetLeft(ImgContentCtrl, (left + e.HorizontalChange))
+        Canvas.SetTop(ImgContentCtrl, (top + e.VerticalChange))
+    End Sub
+
+    Private Sub Img_MouseWheel(ByVal sender As Object, ByVal e As System.Windows.Input.MouseWheelEventArgs)
+        'TODO: Add event handler implementation here.
+
+        ' Variable for holding the mouse's delta value.
+        Dim deltaValue As Integer
+
+        deltaValue = e.Delta
+
+        ' Set the center point of the ScaleTransform object
+        ' to the cursor location.
+        myScale.CenterX = e.GetPosition(ImgContentCtrl).X
+        myScale.CenterY = e.GetPosition(ImgContentCtrl).Y
+
+        ' Zoom in when the user scrolls the mouse wheel up
+        ' and vice versa.
+        If deltaValue > 0 Then
+            ' Limit zoom-in to 500%
+            If myScale.ScaleX < 5 Then
+                ' Zoom-in in 10% increments
+                myScale.ScaleX += 0.1
+                myScale.ScaleY += 0.1
+            End If
+            ' When mouse wheel is scrolled down...
+        Else
+            ' Limit zoom-out to 80%
+            If myScale.ScaleX > 0.8 Then
+                ' Zoom-out by 10%
+                myScale.ScaleX -= 0.1
+                myScale.ScaleY -= 0.1
+            End If
+        End If
+    End Sub
+
+    Private Sub OpenButton_Click(ByVal sender as Object, ByVal e as System.Windows.RoutedEventArgs)
 		'TODO: Add event handler implementation here.
 		Dim OpenDialog as New OpenFileDialog
 		
