58c65085d377e0b8562ab247e1828064307a5ea4
AdiTCG <adithecomputerguy@gmail.com>
2015-12-15 00:06:24 +0530

    String algo beta

---
+++ FindStringAlgo_Beta/FindStringAlgo_Beta/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindStringAlgo_Beta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool MatchFound = false;
+            string T = "MyNameIs", p = "Name";
+            int n = T.Count(), m = p.Count();
+            for (int i = 0; i < n - m; i++)
+            {
+                int j = 0;
+                while (j < m && p[j] == T[i + j])
+                {
+                    j++;
+                }
+                if (j == m)
+                {
+                    Console.Write("Match Found\n", i);
+                    MatchFound = true;
+                }
+            }
+            if (!MatchFound)
+                Console.Write("No Match Found\n");
+        }
+    }
+}
